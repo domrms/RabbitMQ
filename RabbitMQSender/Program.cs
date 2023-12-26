b88a7cc647b822e9a204fec1a81807d7016270ca
Domingos Alves <73261252+domrms@users.noreply.github.com>
2023-12-26 10:01:41 -0300

    Rascunho.

--- RabbitMQSender/Program.cs
+++ RabbitMQSender/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace RabbitMQSender
+﻿using RabbitMQ.Client;
+
+namespace RabbitMQSender
 {
     public class Program
     {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 15672 };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "filatestes",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+        }
 
     }
 }
